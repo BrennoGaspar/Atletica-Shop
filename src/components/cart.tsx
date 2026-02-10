2a31a2e0e48bd88cbdf8825c6d6d87ab848e5199
BrennoGaspar <brennogasparpinto@gmail.com>
2026-02-09 22:30:46 -0300

    Fetch items in database for shopping cart

--- src/components/cart.tsx
+++ src/components/cart.tsx
@@ -1,8 +1,9 @@
 'use client'
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Dialog, DialogBackdrop, DialogPanel, DialogTitle } from '@headlessui/react'
 import { XMarkIcon } from '@heroicons/react/24/outline'
+import { supabase } from '@/lib/supabase'
 
 const products = [
   {
@@ -38,12 +39,53 @@ const products = [
   },
 ]
 
+const userId = 1
+let dataGeneral: any[]
+
+
+
 interface CartProps {
   open: boolean;
   setOpen: (open: boolean) => void;
 }
 
 export default function PersonalCart({ open, setOpen }: CartProps) {
+
+  const [cartItems, setCartItems] = useState<any[]>([])
+
+  async function FetchData() {
+  const { data, error } = await supabase
+    .from('cart_items')
+    .select(`
+      id,
+      quantity,
+      products (
+        id,
+        name,
+        price
+      )
+    `)
+    .eq('user_id', userId)
+
+  if (error) {
+    console.log('Erro ao carregar carrinho: ', error)
+    return
+  } else {
+    console.log(data)
+  }
+
+  setCartItems(data || []);
+}
+
+  useEffect(() => {
+    if( open ){
+      FetchData()
+    }
+  }, [open])
+
+  const subtotal = cartItems.reduce((acc, item) => {
+    return acc + (item.products?.price * item.quantity)
+  }, 0)
 
   return (
     <div>
@@ -63,7 +105,7 @@ export default function PersonalCart({ open, setOpen }: CartProps) {
                 <div className="flex h-full flex-col overflow-y-auto bg-white shadow-xl">
                   <div className="flex-1 overflow-y-auto px-4 py-6 sm:px-6">
                     <div className="flex items-start justify-between">
-                      <DialogTitle className="text-lg font-medium text-gray-900">Shopping cart</DialogTitle>
+                      <DialogTitle className="text-lg font-medium text-gray-900">Carrinho</DialogTitle>
                       <div className="ml-3 flex h-7 items-center">
                         <button
                           type="button"
@@ -71,7 +113,7 @@ export default function PersonalCart({ open, setOpen }: CartProps) {
                           className="relative -m-2 p-2 text-gray-400 hover:text-gray-500"
                         >
                           <span className="absolute -inset-0.5" />
-                          <span className="sr-only">Close panel</span>
+                          <span className="sr-only">Fechar</span>
                           <XMarkIcon aria-hidden="true" className="size-6" />
                         </button>
                       </div>
@@ -80,24 +122,21 @@ export default function PersonalCart({ open, setOpen }: CartProps) {
                     <div className="mt-8">
                       <div className="flow-root">
                         <ul role="list" className="-my-6 divide-y divide-gray-200">
-                          {products.map((product) => (
-                            <li key={product.id} className="flex py-6">
+                          {cartItems.map((item) => (
+                            <li key={item.id} className="flex py-6">
                               <div className="size-24 shrink-0 overflow-hidden rounded-md border border-gray-200">
-                                <img alt={product.imageAlt} src={product.imageSrc} className="size-full object-cover" />
+                                <img alt='Imagem' src='' className="size-full object-cover" />
                               </div>
 
                               <div className="ml-4 flex flex-1 flex-col">
-                                <div>
-                                  <div className="flex justify-between text-base font-medium text-gray-900">
-                                    <h3>
-                                      <a href={product.href}>{product.name}</a>
-                                    </h3>
-                                    <p className="ml-4">{product.price}</p>
-                                  </div>
-                                  <p className="mt-1 text-sm text-gray-500">{product.color}</p>
+                                <div className="flex justify-between text-base font-medium text-gray-900">
+                                  <h3>
+                                    <a>{item.products.name}</a>
+                                  </h3>
+                                  <p className="ml-4">{item.products.price}</p>
                                 </div>
                                 <div className="flex flex-1 items-end justify-between text-sm">
-                                  <p className="text-gray-500">Qty {product.quantity}</p>
+                                  <p className="text-gray-500">Qty {item.quantity}</p>
 
                                   <div className="flex">
                                     <button type="button" className="font-medium text-indigo-600 hover:text-indigo-500">
@@ -115,8 +154,8 @@ export default function PersonalCart({ open, setOpen }: CartProps) {
 
                   <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
                     <div className="flex justify-between text-base font-medium text-gray-900">
-                      <p>Subtotal</p>
-                      <p>$262.00</p>
+                      <p>Total</p>
+                      <p>R${subtotal}</p>
                     </div>
                     <p className="mt-0.5 text-sm text-gray-500">Shipping and taxes calculated at checkout.</p>
                     <div className="mt-6">
@@ -124,7 +163,7 @@ export default function PersonalCart({ open, setOpen }: CartProps) {
                         href="#"
                         className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-xs hover:bg-indigo-700"
                       >
-                        Checkout
+                        Ir para pagamento
                       </a>
                     </div>
                   </div>
@@ -136,4 +175,7 @@ export default function PersonalCart({ open, setOpen }: CartProps) {
       </Dialog>
     </div>
   )
+}
+function setCartItems(arg0: any[]) {
+  throw new Error('Function not implemented.')
 }
