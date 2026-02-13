d218417102bff547f60021b63dabe16658e1c156
BrennoGaspar <brennogasparpinto@gmail.com>
2026-02-13 00:02:24 -0300

    Added payment page and back-end system

--- src/components/loginForm.tsx
+++ src/components/loginForm.tsx
@@ -21,9 +21,9 @@ export default function LoginForm({ title, buttonMessage, button, onSubmitAction
             <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
                 <form onSubmit={onSubmitAction} className="space-y-6">
                     <div>
-                        <label htmlFor="email" className="block text-sm/6 font-medium text-gray-100">email</label>
+                        <label htmlFor="email" className="block text-sm/6 font-medium text-gray-100">Email</label>
                         <div className="mt-2">
-                            <input id="email" type="email" name="email" required autoComplete="email" className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
+                            <input id="email" placeholder="seuemail@dominio.com" type="email" name="email" required autoComplete="email" className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
                         </div>
                     </div>
 
@@ -35,7 +35,7 @@ export default function LoginForm({ title, buttonMessage, button, onSubmitAction
                                 </div> */}
                         </div>
                         <div className="mt-2">
-                            <input id="password" type="password" name="password" required autoComplete="current-password" className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
+                            <input id="password" type="password" placeholder="***" name="password" required autoComplete="current-password" className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
                         </div>
                     </div>
 
--- src/components/registerForm.tsx
+++ src/components/registerForm.tsx
@@ -22,7 +22,7 @@ export default function RegisterForm({ title, buttonMessage, onSubmitAction }: P
                     <div>
                         <label htmlFor="email" className="block text-sm/6 font-medium text-gray-100">Nome</label>
                         <div className="mt-2">
-                            <input id="email" type="text" name="name" required className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
+                            <input id="email" type="text" placeholder="Seu Nome" name="name" required className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
                         </div>
                     </div>
 
@@ -31,7 +31,7 @@ export default function RegisterForm({ title, buttonMessage, onSubmitAction }: P
                             <label htmlFor="age" className="block text-sm/6 font-medium text-gray-100">Idade</label>
                         </div>
                         <div className="mt-2">
-                            <input id="age" type="text" name="age" required className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
+                            <input id="age" type="text" name="age" placeholder="Idade" required className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
                         </div>
                     </div>
 
@@ -40,7 +40,7 @@ export default function RegisterForm({ title, buttonMessage, onSubmitAction }: P
                             <label htmlFor="email" className="block text-sm/6 font-medium text-gray-100">Email</label>
                         </div>
                         <div className="mt-2">
-                            <input id="email" type="email" name="email" required autoComplete="email" className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
+                            <input id="email" type="email" placeholder="seuemail@dominio.com" name="email" required autoComplete="email" className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
                         </div>
                     </div>
 
@@ -49,7 +49,7 @@ export default function RegisterForm({ title, buttonMessage, onSubmitAction }: P
                             <label htmlFor="phone" className="block text-sm/6 font-medium text-gray-100">Número para contato</label>
                         </div>
                         <div className="mt-2">
-                            <input id="phone" type="phone" name="phone" required className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
+                            <input id="phone" type="phone" name="phone" placeholder="(11) 988887777" required className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
                         </div>
                     </div>
 
@@ -58,7 +58,7 @@ export default function RegisterForm({ title, buttonMessage, onSubmitAction }: P
                             <label htmlFor="password" className="block text-sm/6 font-medium text-gray-100">Senha</label>
                         </div>
                         <div className="mt-2">
-                            <input id="password" type="password" name="password" required className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
+                            <input id="password" type="password" placeholder="***" name="password" required className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
                         </div>
                     </div>
 
